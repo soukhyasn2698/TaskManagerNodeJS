8804aba6eca22947ec421296976d7a47ce9e9119
soukhyasn2698 <soukhyamn17@gmail.com>
2026-03-14 16:41:51 -0400

    Updated app js

--- app.js
+++ app.js
@@ -97,4 +97,8 @@ app.delete("/tasks/:taskId", async (req, res) => {
 app.get("/", (req, res) => {
   res.send("Task Manager API Running");
 });
+const PORT = 3000;
 
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
